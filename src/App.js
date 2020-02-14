de0ee037f02d10535269bc1017f62d8bc0c4025b
Swizec Teller <swizec@swizec.com>
2020-02-14 11:17:23 -0500

    Embed tweets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const query = new URLSearchParams(window.location.search);
+        const tweetUrl = query.get("url");
+
+        if (tweetUrl) {
+            const id = tweetUrl.split("/status/")[1];
+
+            window.twttr.widgets.createTweet(
+                id,
+                document.getElementById("tweet"),
+                {
+                    cards: true
+                }
+            );
+        }
+    }, []);
+
+    return (
+        <div className="App">
+            <div id="tweet"></div>
+        </div>
+    );
 }
 
 export default App;
